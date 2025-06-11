154f03f7f4b9e62ce17619b1637670a239385940
Adarsh Shrivastav <aadarshshrivastav01@gmail.com>
2025-06-11 14:52:35 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 left-0 w-full bg-white text-black py-4 text-center z-50">
+      <p className="text-sm">
+        Made by <span className="font-semibold">Aadarsh Shrivastav</span>,{" "}
+        <span className="font-semibold">Prapti Churi</span> &{" "}
+        <span className="font-semibold">Dhvani Sawani</span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
